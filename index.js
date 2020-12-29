fea47138cb67af70a03e73b1a7e3c53d58ca9f81
jainshubham785 <jainshubham785@gmail.com>
2020-12-30 01:47:39 +0530

    version 1.0

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(val) {
+    var x = document.querySelector("input").getAttribute("value");
+    document.querySelector("input").setAttribute("value", x + "" + val);
+}
+
+function equals() {
+    var exp = document.querySelector("input").getAttribute("value");
+    var num;
+
+    if(exp.includes("+")) {
+        num = exp.split("+");
+        document.querySelector("input").setAttribute("value", parseInt(num[0]) + parseInt(num[1]));
+    }
+
+    else if(exp.includes("-")) {
+        num = exp.split("-");
+        document.querySelector("input").setAttribute("value", parseInt(num[0]) - parseInt(num[1]));
+    }
+
+    else if(exp.includes("*")) {
+        num = exp.split("*");
+        document.querySelector("input").setAttribute("value", parseInt(num[0]) * parseInt(num[1]));
+    }
+    
+    else {
+        num = exp.split("/");
+        document.querySelector("input").setAttribute("value", parseInt(num[0]) / parseInt(num[1]));
+    }
+}
+
+function reset() {
+    document.querySelector("input").setAttribute("value", "");
+}
